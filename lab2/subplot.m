add5dec0656a03363215ed525ebe184579df162c
Asim Poudel <asimdkt63@gmail.com>
2025-05-26 14:16:14 +0545

    Update and rename subplot.m to lab2/subplot.m

--- lab2/subplot.m
+++ lab2/subplot.m
@@ -1,25 +1,25 @@
 t = 0:0.01:2;
-y = sin(2*3.1415*t)
+y = sin(2*3.1415*t);
 subplot(2,2,1);
 plot(t,y);
-title('Continuous sine wave/Asim/08');
+title('Harmonic 1/Asim/08');
 xlabel('Time(s)');
 ylabel('Amplitude');
 grid on;
 
 
-z = sin(2*3.1415*2*t)
+z = sin(2*3.1415*2*t);
 subplot(2,2,2);
 plot(t,z);
-title('Continuous sine wave/Asim/08');
+title('Harmonic 2/Asim/08');
 xlabel('Time(s)');
 ylabel('Amplitude');
 grid on;
 
-g = cos(2*3.1415*3*t)
+g = cos(2*3.1415*3*t);
 subplot(2,2,3);
 plot(t,g);
-title('Continuous cosine wave/Asim/08');
+title('Harmonic 3/Asim/08');
 xlabel('Time(s)');
 ylabel('Amplitude');
 grid on;
@@ -27,8 +27,7 @@ grid on;
 x = y+g+z;
 subplot(2,2,4);
 plot(t,x);
-title('SUM /Asim/ 08');
+title('Composite /Asim/ 08');
 xlabel('time');
 ylabel('amplitude');
 grid on;
-
